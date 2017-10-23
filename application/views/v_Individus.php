6cc722ee14b097994ef5185bccc7c6ab72c33a09
Axel <Axel@172.16.0.133>
2017-10-23 21:02:26 +0200

    fusion2.0

--- application/views/v_Individus.php
+++ application/views/v_Individus.php
@@ -3,8 +3,8 @@
         <title>Individu</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <script type="text/javascript" src="JQuery/jquery-3.1.1.js"></script>
-         <script type="text/javascript" src="JS/lesFonctions.js"></script>
+         <script type="text/javascript" src="<?php echo base_url();?>JQuery/jquery-3.1.1.js"></script>
+         <script type="text/javascript" src="<?php echo base_url();?>JS/lesFonctions.js"></script>  
          <script type="text/javascript">
          
               $
