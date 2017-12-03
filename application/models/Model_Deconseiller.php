c5137d441a5a16305c478f5132018a00985ce19b
Axel <Axel@172.16.0.133>
2017-12-03 19:43:58 +0100

    Afficher en cliquant la liste des medicaments déconseillé

--- application/models/Model_Deconseiller.php
+++ application/models/Model_Deconseiller.php
@@ -15,7 +15,7 @@ class Model_Deconseiller extends CI_Model{
       return $sql -> result();
         
     }
-         public function GetComposantDuMedicament($MED_PERTURBE)
+         public function GetMedicamentDecon($MED_PERTURBE)
             {
                  $sql = $this->db->query("SELECT MED_MED_PERTURBE FROM interagir WHERE MED_PERTURBE='".$MED_PERTURBE."'");
                   return $sql->result();
